7f8b6597b826d2defb5dbdfd52545c5ae7088686
Michael Czechowski <mail@dailysh.it>
2021-06-30 16:04:05 +0200

    deploy to github pages

---
+++ precache-manifest.8ed98296ba8f1ae48f81be6e09c6fa42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b2c4511cfbfb0cedf8",
+    "url": "/css/app.cdecf25c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "64ca40e1adace35387d6875eb6f0dcf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b2c4511cfbfb0cedf8",
+    "url": "/js/app.611aefe0.js"
+  },
+  {
+    "revision": "0256ac2581e0f37f334b",
+    "url": "/js/chunk-vendors.3974b1ce.js"
+  },
+  {
+    "revision": "2cac219171d46744ebc437e2ae577707",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

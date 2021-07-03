33b8f5aa28819073abf40c39e1c76c3016e78471
Michael Czechowski <mail@dailysh.it>
2021-07-03 17:06:12 +0200

    add watchlist (1h)

---
+++ src/components/Watchlist.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+    <li
+      v-for="product in products"
+      :key="product.isin"
+      @click="removeProduct(product.isin)"
+    >
+      {{ product.displayName }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Watchlist",
+  props: {
+    products: Array,
+    isStale: Boolean,
+  },
+  methods: {
+    removeProduct: (isin: string): void => {
+      console.log(isin);
+    },
+  },
+  updated(): void {
+    console.log("updated");
+  },
+});
+</script>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  > li {
+    padding: 1rem;
+
+    + li {
+      border-top: 1px solid #c8c8c8;
+    }
+  }
+}
+</style>
